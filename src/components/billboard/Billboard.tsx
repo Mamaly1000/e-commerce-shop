5a7571dc1028e84386c801742ac93a474ff941e1
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-04-17 04:11:30 +0330

    fix billboard ui and color blending

--- src/components/billboard/Billboard.tsx
+++ src/components/billboard/Billboard.tsx
@@ -10,11 +10,11 @@ const Billboard: FC<props> = ({ billboard }) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover "
         style={{ backgroundImage: `url(${billboard?.poster})` }}
       >
         <div className="h-full w-full flex flex-col items-center justify-center  text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl bg-white/20 dark:bg-black/50 min-w-full max-w-full px-5 md:px-10 h-full flex items-center justify-center">
             {billboard?.label}
           </div>
         </div>
