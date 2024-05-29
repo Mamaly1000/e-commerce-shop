aa29425aeacd0a5d268f44b4014d8bbe318162d6
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-05-29 20:53:17 +0330

    last v

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -16,10 +16,10 @@ export interface store_with_analytic {
   name: string;
   description: string;
   background_Image: string;
-  logo: string;
+  logo: string; 
   total_revenue: number;
   total_sell_products: number;
-  id: string;
+  id: string; 
 }
 
 export interface Store {
